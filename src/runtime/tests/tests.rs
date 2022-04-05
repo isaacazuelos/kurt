d9cc790edce045d674dba6c3f83cb59daf8d8bcf
Isaac Azuelos <isaac@azuelos.ca>
2022-04-04 19:47:07 -0600

    Add simple runtime eval tests

---
+++ src/runtime/tests/tests.rs
@@ -0,0 +1,29 @@
+//! Test evaluation of pieces of code to make sure things work as expected.
+
+macro_rules! test_eval {
+    ($name: ident, $input: expr, $expected: expr) => {
+        #[test]
+        fn $name() {
+            let obj = compiler::compile($input).unwrap();
+            let mut rt = runtime::Runtime::new(obj).unwrap();
+            rt.start().unwrap();
+            assert_eq!(rt.last_result(), $expected);
+        }
+    };
+}
+
+// Sanity check
+test_eval! { constants, "1", "1" }
+
+// Statement sequences
+test_eval! { empty_input, "", "()" }
+test_eval! { semicolons_only, ";;;;;", "()" }
+test_eval! { no_trailing_semicolon, "1;2;3", "3" }
+test_eval! { trailing_semicolon, "1;2;3;", "()" }
+
+// Local `let` bindings
+test_eval! { let_return_unit, "let x = 1", "()" }
+test_eval! { let_local_lookup, "let x = 1; x", "1" }
+test_eval! { let_local_twice, "let x = 1; let y = 2; x", "1" }
+test_eval! { let_local_shadow, "let x = 1; let x = 2; x", "2" }
+test_eval! { let_local_complex, "let x = 1; 100; ;;; let y = 2; x; 10; y", "2" }
